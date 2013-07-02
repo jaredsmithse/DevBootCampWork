1137427fec8021c32f40677fc8df8c3f2a989afe
Jared Smith <jared.rit@me.com>
2013-07-02 16:20:32 -0700

    Completed jquery ajax challenge.

--- assessments/jquery_ajax/app/controllers/index.rb
+++ assessments/jquery_ajax/app/controllers/index.rb
@@ -4,9 +4,7 @@ get '/' do
 end
 
 post '/color' do
-
-  #Create and return a JSON object with the random cell and color given below.
-
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+  {cell: cell, color: color}.to_json
 end
--- assessments/jquery_ajax/public/js/application.js
+++ assessments/jquery_ajax/public/js/application.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
+function changeCellColor(serverResponse) {
+	var data = $.parseJSON(serverResponse)
+	$('#color_me').children('li').eq(data.cell-1).css('background',data.color);
+}
 
+$(document).ready(function(){
+	$('#get_color').click(function(evt) {
+		var input = $(this).serialize()
+		evt.preventDefault()
+		evt.preventDefault();
+		$.post('/color', input, changeCellColor);
+	});
 });
