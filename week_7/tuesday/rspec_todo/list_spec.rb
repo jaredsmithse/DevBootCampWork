dcaaaf2c56cddd0be0ed5e208101c3f882ccd9db
Jared Smith <jared.rit@me.com>
2013-07-22 14:28:58 -0700

    Uncommitted changes

--- week_7/tuesday/rspec_todo/list_spec.rb
+++ week_7/tuesday/rspec_todo/list_spec.rb
@@ -3,5 +3,72 @@ require "rspec"
 require_relative "list"
 
 describe List do
-  # Your specs here
+  let(:list) { List.new("test") }
+
+  describe "#initialize" do
+
+  	it "should default tasks to an empty array if none are given" do
+      expect(List.new("test").tasks).to eq []
+    end
+
+  end
+
+
+  describe "#title" do
+    it "should be readable" do
+      expect(list.title).to eq "test"
+    end
+
+    it "should not be editable" do
+      expect {list.title = "new test"}.to raise_error NoMethodError
+    end
+
+  end
+
+
+  describe "#tasks" do
+    it "should be readable" do
+      expect(list.tasks).to eq []
+    end
+
+    it "should not be editable" do 
+      
+    end
+
+  end
+
+
+  describe "#add_task" do
+    it "should add a given task to the tasks list"
+
+  end
+
+
+  describe "#complete_task" do
+    it "should mark the given task as complete"
+
+  end
+
+
+  describe "#delete_tasks" do
+    it "should delete the given task from the tasks list"
+
+  end
+
+
+  describe "#completed_tasks" do
+    it "should find all completed tasks"
+
+    it "should not have any incomplete tasks in the returned array"
+
+  end
+
+
+  describe "#incomplete_tasks" do
+    it "should have all of the incomplete tasks returned"
+
+    it "should not have any complete tasks returned"
+
+  end
+
 end
