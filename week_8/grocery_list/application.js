3f5cc54152a726a9f77eaf955d4a9091d6adfd70
Jared Smith <jared.rit@me.com>
2013-07-22 11:21:02 -0700

    Add challenge for jquery ui drag and drop

---
+++ week_8/grocery_list/application.js
@@ -0,0 +1,6 @@
+/* 
+ * What are the objects in this exercise?
+ * What are their properties and methods?
+ * How do your objects interact with their respective DOM elements?
+ */
+//this will hold reference to the tr we have dragged and its helper
