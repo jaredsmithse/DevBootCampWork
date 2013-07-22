050ac892b54d9791c2282c3e8ae28cf2928ecd3c
Jared Smith <jared.rit@me.com>
2013-07-22 14:28:18 -0700

    Finish challenge, did not do OOjs (too little code) but did make list sortable

--- week_8/grocery_list/application.js
+++ week_8/grocery_list/application.js
@@ -4,3 +4,30 @@
  * How do your objects interact with their respective DOM elements?
  */
 //this will hold reference to the tr we have dragged and its helper
+
+$(document).ready ( function() {
+	$('#store_list .item').draggable({
+		helper: 'clone',
+		appendTo: 'body'
+	});
+
+	$('#grocery_list').droppable({ 
+		accept: '.item',
+		drop: function(event, ui) { 
+			var element = ui.draggable.clone();
+			element.removeClass('item');
+			$(this).append(element);
+			reCalculateTotal();
+		}  
+	});
+
+	$('#grocery_list tbody').sortable();
+
+	function reCalculateTotal() {
+		var total = 0;
+		$('#grocery_list tbody tr').each( function() {
+			total += parseFloat($(this).find('td.item_price').text());
+		})
+		$('#total_cost').text(total)
+	}
+});
